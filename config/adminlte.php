bc50464a6cfe1c76c60c98a528d2688fa119a6b4
brunobar88 <bruno.zanella2015@outlook.com>
2020-02-10 22:15:14 -0300

    adicionado permissao de super usuario

--- config/adminlte.php
+++ config/adminlte.php
@@ -186,7 +186,8 @@ return [
         [
             'text'        => 'Meus Usuários',
             'url'         => 'painel/users',
-            'icon'        => 'far fa-fw fa-file'
+            'icon'        => 'far fa-fw fa-file',
+            'can'         => 'edit-users'
         ],      
 
         ['header' => 'account_settings'],
