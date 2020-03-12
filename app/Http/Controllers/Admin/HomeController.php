f493b3a553c7613ca3844b94cd32a1230b9d3325
brunobar88 <bruno.zanella2015@outlook.com>
2020-03-11 22:26:24 -0300

    (adicionado numeros dinamicos a dashboard) -feito contagem de estimativa de usuarios online -contagem de usuarios -contagem de paginas ativas -contagem de visitas feitas as paginas -coloocado informaçoes dinamicas na view

--- app/Http/Controllers/Admin/HomeController.php
+++ app/Http/Controllers/Admin/HomeController.php
@@ -3,12 +3,30 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Page;
+use App\User;
+use App\visitor;
 use Illuminate\Support\Facades\Auth;
 
 class HomeController extends Controller
 {
+    public function __construct() {
+        $this->middleware('auth');
+    }
+
     public function index()
     {
-        return view('admin.home');
+        $dateLimit = date('Y-m-d H:i:s', strtotime('-5 minutes'));
+        $listVisitors = Visitor::select('ip')->where('access_date', '>=', $dateLimit)->groupBy('ip')->get();
+        $online = count($listVisitors);
+
+        $infos = [
+            'visitCount' => visitor::count(),
+            'onlineCount' => $online,
+            'pageCount' => Page::count(),
+            'userCount' => User::count(), 
+        ];
+
+        return view('admin.home', $infos);
     }
 }
