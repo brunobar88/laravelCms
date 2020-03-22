b99592dc430f56b056629bf1a38a63cb990d4a7e
Bruno B. Zanella <bruno.zanella2015@outlook.com>
2020-03-21 22:15:55 -0300

    (template)
    -adicionado template da frente do site
    -implementado template ao laravel-
    -adicionado links para as páginas dinâmicos

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Page;
+use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -23,6 +25,15 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        $frontMenu = [
+            '/' => 'Home',
+        ];
+
+        $pages = Page::all();
+        foreach ($pages as $page) {
+            $frontMenu[ $page['slug'] ] = $page['title'];
+        }
+         
+        View::share('front_menu', $frontMenu);
     }
 }
